9fabdc386c24bb81f8d4de5d781dfa965104622c
Joe-noh <goflb.jh@gmail.com>
2017-06-03 00:12:00 +0900

    launch 24 hours ttl store

--- lib/nyon/application.ex
+++ lib/nyon/application.ex
@@ -1,23 +1,21 @@
 defmodule Nyon.Application do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec
 
-    # Define workers and child supervisors to be supervised
-    children = [
-      # Start the Ecto repository
-      supervisor(Nyon.Repo, []),
-      # Start the endpoint when the application starts
-      supervisor(Nyon.Web.Endpoint, []),
-      # Start your own worker by calling: Nyon.Worker.start_link(arg1, arg2, arg3)
-      # worker(Nyon.Worker, [arg1, arg2, arg3]),
+    cache_opts = [
+      ttl_check: :timer.seconds(10),
+      ttl: :timer.hours(24)
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
+    children = [
+      supervisor(Nyon.Repo, []),
+      supervisor(Nyon.Web.Endpoint, []),
+
+      worker(ConCache, [cache_opts, [name: {:global, :posts}]]),
+    ]
+
     opts = [strategy: :one_for_one, name: Nyon.Supervisor]
     Supervisor.start_link(children, opts)
   end
