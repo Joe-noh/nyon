963c63d1fa08702810ca52e81708eb5fd7b9e82d
Joe-noh <goflb.jh@gmail.com>
2017-06-03 01:43:16 +0900

    lough implementation

--- lib/nyon/web/controllers/post_controller.ex
+++ lib/nyon/web/controllers/post_controller.ex
@@ -1,11 +1,35 @@
 defmodule Nyon.Web.PostController do
   use Nyon.Web, :controller
 
+  @cache {:global, :posts}
+
   def index(conn, _params) do
+    posts = @cache
+      |> ConCache.ets
+      |> :ets.tab2list
+      |> Enum.map(fn {_key, post} -> post end)
+
     conn
+    |> put_status(200)
+    |> json(%{posts: posts})
   end
 
   def create(conn, %{"body" => body}) do
+    body = String.trim(body)
+    post = %{body: body}
+
+    if String.length(body) > 0 do
+      ConCache.put(@cache, random_string(), post)
+    end
+
     conn
+    |> put_status(201)
+    |> json(%{post: post})
+  end
+
+  defp random_string do
+    :crypto.strong_rand_bytes(64)
+    |> Base.url_encode64
+    |> binary_part(0, 64)
   end
 end
