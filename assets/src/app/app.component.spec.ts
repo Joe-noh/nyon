332db5e6fd6367bd0b8c1ba11105c9b6b53182ed
Joe-noh <goflb.jh@gmail.com>
2017-06-04 00:08:33 +0900

    fetch and list all posts

--- assets/src/app/app.component.spec.ts
+++ assets/src/app/app.component.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
+import { MockBackend } from '@angular/http/testing';
 
+import { Http, ConnectionBackend, RequestOptions, BaseRequestOptions } from '@angular/http';
 import { AppComponent } from './app.component';
+import { PostService } from './services/post.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,25 +11,19 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        {provide: ConnectionBackend, useClass: MockBackend},
+        {provide: RequestOptions, useClass: BaseRequestOptions},
+        Http,
+        PostService
+      ]
     }).compileComponents();
   }));
 
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'nyon'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('nyon');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to nyon!!');
   }));
 });
--- assets/src/app/app.component.ts
+++ assets/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { PostService, Post } from './services/post.service';
 
 @Component({
   selector: 'nyon-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'nyon';
+export class AppComponent implements OnInit {
+  private posts: Post[];
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  fetchPosts(): void {
+    this.postService.fetchAll().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
 }
