bf1c5c3a5ab2a25e03a184d17085639aef189180
Joe-noh <goflb.jh@gmail.com>
2017-06-04 11:15:35 +0900

    lack of FormsModule in test

--- assets/src/app/app.component.spec.ts
+++ assets/src/app/app.component.spec.ts
@@ -2,12 +2,15 @@ import { TestBed, async } from '@angular/core/testing';
 import { MockBackend } from '@angular/http/testing';
 
 import { Http, ConnectionBackend, RequestOptions, BaseRequestOptions } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { PostService } from './services/post.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [
         AppComponent
       ],
