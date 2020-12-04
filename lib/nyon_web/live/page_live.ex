d9d693797bea246eb8d624c0d070400c5c46e2ab
Joe-noh <goflb.jh@gmail.com>
2020-12-05 01:38:21 +0900

    implement counter with LiveView

--- lib/nyon_web/live/page_live.ex
+++ lib/nyon_web/live/page_live.ex
@@ -3,7 +3,7 @@ defmodule NyonWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    {:ok, assign(socket, query: "", results: %{}, count: 0)}
   end
 
   @impl true
@@ -23,6 +23,13 @@ defmodule NyonWeb.PageLive do
          |> put_flash(:error, "No dependencies found matching \"#{query}\"")
          |> assign(results: %{}, query: query)}
     end
+  end
+
+  @impl true
+  def handle_event("inc", %{"diff" => value}, socket) do
+    value = String.to_integer(value)
+    socket = assign(socket, count: socket.assigns.count + value)
+    {:noreply, socket}
   end
 
   defp search(query) do
