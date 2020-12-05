5fad244311fa7a6de6bf3c542e07579d59b95b0e
Joe-noh <goflb.jh@gmail.com>
2020-12-05 18:20:08 +0900

    remove a test no longer needed

--- test/nyon_web/live/page_live_test.exs
+++ test/nyon_web/live/page_live_test.exs
@@ -3,12 +3,6 @@ defmodule NyonWeb.PageLiveTest do
 
   import Phoenix.LiveViewTest
 
-  test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
-  end
-
   test "increment counter", %{conn: conn} do
     {:ok, view, _html} = live(conn, "/")
 
