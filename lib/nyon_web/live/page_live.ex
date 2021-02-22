11a65d99f596315a54f66846e5343913dd7fa6bb
Joe-noh <goflb.jh@gmail.com>
2021-02-22 23:01:31 +0900

    remove spotify related code for now

--- lib/nyon_web/live/page_live.ex
+++ lib/nyon_web/live/page_live.ex
@@ -1,58 +1,11 @@
 defmodule NyonWeb.PageLive do
   use NyonWeb, :live_view
 
-  alias Nyon.Identities
-
   @impl true
-  def mount(_params, %{"current_user_id" => user_id}, socket) do
-    {:ok, current_user} = Identities.find_user(user_id)
-    send(self(), :get_player)
-
+  def mount(_params, _session, socket) do
     :ok = Phoenix.PubSub.subscribe(Nyon.PubSub, "board:1")
 
-    socket =
-      socket
-      |> assign(:user, current_user)
-      |> assign(:device, nil)
-      |> assign(:device_state, :loading)
-
     {:ok, socket}
-  end
-
-  def mount(_params, _session, socket) do
-    {:ok, redirect(socket, to: Routes.signin_path(socket, :index))}
-  end
-
-  @impl true
-  def handle_event("play", _, socket = %{assigns: %{user: user, device: device}}) do
-    case device do
-      nil ->
-        :do_nothing
-
-      %Sptfy.Object.Device{id: id} ->
-        {:ok, account} = Identities.refresh_if_expired(user.spotify_account)
-        Sptfy.Player.play(account.access_token, uris: ["spotify:track:5MimWt53Ukh0gcv7mC0Rnx"], device_id: id)
-    end
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info(:get_player, socket = %{assigns: %{user: user}}) do
-    {:ok, account} = Identities.refresh_if_expired(user.spotify_account)
-
-    case Sptfy.Player.get_devices(account.access_token) do
-      {:ok, devices = [device | _]} ->
-        device = find_active_device(devices) || device
-        socket = assign(socket, :device, device)
-        {:noreply, socket}
-
-      {:ok, []} ->
-        {:noreply, socket}
-
-      {:error, _} ->
-        {:noreply, socket}
-    end
   end
 
   @impl true
@@ -60,9 +13,5 @@ defmodule NyonWeb.PageLive do
     send_update(NyonWeb.Components.MinesweeperBoard, id: 1)
 
     {:noreply, socket}
-  end
-
-  defp find_active_device(devices) do
-    Enum.find(devices, fn d -> d.is_active end)
   end
 end
