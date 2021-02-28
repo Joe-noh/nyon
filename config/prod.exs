f90708648b99fe744bea777543c1aeab74576aa4
Joe-noh <goflb.jh@gmail.com>
2021-02-28 14:16:36 +0900

    remove release.exs

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,9 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :nyon, NyonWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "nyon.gigalixirapp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
