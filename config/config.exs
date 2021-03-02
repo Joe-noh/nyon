fb79ee2603c5ada6b028413360e01b15d6b2d9f7
Joe-noh <goflb.jh@gmail.com>
2021-03-03 01:06:02 +0900

    need user-top-read scope

--- config/config.exs
+++ config/config.exs
@@ -23,7 +23,7 @@ config :nyon, :spotify,
   client_id: System.get_env("SPOTIFY_CLIENT_ID"),
   client_secret: System.get_env("SPOTIFY_CLIENT_SECRET"),
   callback_url: System.get_env("SPOTIFY_CALLBACK_URL", "http://localhost:4000/spotify/callback"),
-  scopes: ~w[user-read-playback-state user-modify-playback-state]
+  scopes: ~w[user-read-playback-state user-modify-playback-state user-top-read]
 
 # Configures Elixir's Logger
 config :logger, :console,
